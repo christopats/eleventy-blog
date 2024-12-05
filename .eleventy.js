568ae00658812d701f65459d8ae2a567f1b6d374
christopats <christopattison@gmail.com>
2024-12-05 16:12:27 +0000

    added postcss

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 export default function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/css/style.css");
+  eleventyConfig.addPassthroughCopy("./src/css/tailwind.css");
   eleventyConfig.addPassthroughCopy("./static/logo-nav.png");
   return {
     passthroughFileCOpy: true,
